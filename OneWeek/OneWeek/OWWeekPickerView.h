8ade8d0dc954b50ef31bbe08fd831a9b5e58f34e
Zaki Shaheen <zakishaheen@me.com>
2014-03-24 19:21:42 +0500

    added week logic

--- OneWeek/OneWeek/OWWeekPickerView.h
+++ OneWeek/OneWeek/OWWeekPickerView.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface OWWeekPickerView : UIView
+@interface OWWeekPickerView : UIView< UIScrollViewDelegate>
 
 @end
--- OneWeek/OneWeek/OWWeekPickerView.m
+++ OneWeek/OneWeek/OWWeekPickerView.m
@@ -15,6 +15,9 @@
 @property (nonatomic) int selectedWeek;
 @property (nonatomic) int currentYear;
 @property (nonatomic) int selectedYear;
+@property (nonatomic) int currentPage;
+
+@property (nonatomic, strong) NSMutableArray *weeksToShow;
 
 
 @end
@@ -29,7 +32,26 @@
     return self;
 }
 
-- (UIView *)getView{
+- (UIView *)getViewForDate:(NSDate*)date{
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *startComponents = [calendar components:NSDayCalendarUnit fromDate:date];
+    
+    int startDay = startComponents.day;
+    
+    NSDateComponents *componentsToAdd = [NSDateComponents new];
+    componentsToAdd.day = 7;
+    NSDate *endDate = [calendar dateByAddingComponents:componentsToAdd toDate:date options:0];
+    
+    NSDateComponents *endComponents = [calendar components:NSDayCalendarUnit fromDate:endDate];
+    
+    int endDay = endComponents.day;
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    
+    [dateFormatter setDateFormat:@"MMM ''yy"];
+    
+    NSString *monthString = [[dateFormatter stringFromDate:endDate] uppercaseString];
     
     UIView *result = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 80, 40)];
     
@@ -37,7 +59,7 @@
     NSString *weekFontName = @"HelveticaNeue-Light";
     CGFloat weekFontSize = 12.0f;
     UIColor *weekFontColor = [UIColor darkGrayColor];
-    NSString *weekText = @"10 - 17";
+    NSString *weekText = [NSString stringWithFormat:@"%.2d - %.2d", startDay, endDay];
     
     NSDictionary *weekAttributes = @{
                                  NSFontAttributeName:[ UIFont fontWithName:weekFontName size:weekFontSize],
@@ -57,7 +79,7 @@
     NSString *monthFontName = @"HelveticaNeue-Light";
     CGFloat monthFontSize = 10.0f;
     UIColor *monthFontColor = [UIColor darkGrayColor];
-    NSString *monthText = @"MAR'14";
+    NSString *monthText = monthString;
     
     NSDictionary *monthAttributes = @{
                                       NSFontAttributeName:[ UIFont fontWithName:monthFontName size:monthFontSize],
@@ -79,24 +101,88 @@
     
     return result;
 }
+
+- (void) prepareWeeksToShow{
+    NSCalendar *cal = [NSCalendar currentCalendar];
+    
+    
+    //get today
+    NSDate *today = [NSDate date];
+    NSDateComponents *todayComponents = [cal components:NSYearCalendarUnit |
+                                         NSMonthCalendarUnit |
+                                         NSCalendarUnitWeekOfYear |
+                                         NSDayCalendarUnit |
+                                         NSWeekdayCalendarUnit
+                                               fromDate:today];
+    
+    today = [cal dateFromComponents:todayComponents];
+
+    //normalise to start of week
+    NSDateComponents *subtractiveComponents = [[NSDateComponents alloc] init];
+    [subtractiveComponents setDay:-([todayComponents weekday] - [cal firstWeekday])];
+    [subtractiveComponents setMonth:0];
+    
+    NSDate *beginningOfWeek = [cal dateByAddingComponents:subtractiveComponents toDate:today options:0];
+    
+    
+    NSDate *d = [beginningOfWeek copy];
+    
+    self.weeksToShow = [NSMutableArray new];
+    [self.weeksToShow addObject:d];
+    int i=0;
+    
+    do {
+        NSDateComponents *subComponents = [[NSDateComponents alloc] init];
+        subComponents.weekOfYear = -1;
+        d = [cal dateByAddingComponents:subComponents toDate:d options:0];
+        
+        [self.weeksToShow addObject:d];
+        ++i;
+
+    } while (i<11);
+    
+    [self.weeksToShow sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        NSDate *d1 = (NSDate*)obj1;
+        NSDate *d2 = (NSDate*)obj2;
+        
+        return [d1 compare:d2];
+        
+    }];
+    
+}
+
 - (void)layoutSubviews{
     [super layoutSubviews];
+    
+
+    
     [self setBackgroundColor:[UIColor navigationBarGrey]];
     
     self.navScrollView = [[UIScrollView alloc] initWithFrame:self.bounds];
     [self.navScrollView setPagingEnabled:YES];
     [self.navScrollView setShowsHorizontalScrollIndicator:NO];
     
-    for (int i=0; i<8; i++) {
-        UIView *r = [self getView];
+    [self prepareWeeksToShow];
+    
+    int i=0;
+    for (NSDate *d in self.weeksToShow) {
+        UIView *r = [self getViewForDate:d];
         r.center = CGPointMake(r.center.x + 80*i, r.center.y);
         [self.navScrollView addSubview:r];
+        i++;
     }
     
-    [self.navScrollView setContentSize:CGSizeMake(80*8, 40)];
+    [self.navScrollView setContentSize:CGSizeMake(80* [self.weeksToShow count], 40)];
+    [self.navScrollView setContentOffset:CGPointMake(self.navScrollView.contentSize.width - self.navScrollView.bounds.size.width, 0)];
     
     [self addSubview:self.navScrollView];
 }
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    
+}
+
 
 /*
 // Only override drawRect: if you perform custom drawing.
